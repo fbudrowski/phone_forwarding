544eaa62cf82c059b8aabb6cdc09a0724a1e648a
marpe <marpe@mimuw.edu.pl>
2018-04-14 16:02:44 +0200

    szablon rozwiązania cz. 1 zadania telefony

---
+++ src/phone_forward.h
@@ -0,0 +1,109 @@
+/** @file
+ * Interfejs klasy przechowującej przekierowania numerów telefonicznych
+ *
+ * @author Marcin Peczarski <marpe@mimuw.edu.pl>
+ * @copyright Uniwersytet Warszawski
+ * @date 09.04.2018
+ */
+
+#ifndef __PHONE_FORWARD_H__
+#define __PHONE_FORWARD_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * Struktura przechowująca przekierowania numerów telefonów.
+ */
+struct PhoneForward;
+
+/**
+ * Struktura przechowująca ciąg numerów telefonów.
+ */
+struct PhoneNumbers;
+
+/** @brief Tworzy nową strukturę.
+ * Tworzy nową strukturę niezawierającą żadnych przekierowań.
+ * @return Wskaźnik na utworzoną strukturę lub NULL, gdy nie udało się
+ *         zaalokować pamięci.
+ */
+struct PhoneForward * phfwdNew(void);
+
+/** @brief Usuwa strukturę.
+ * Usuwa strukturę wskazywaną przez @p pf. Nic nie robi, jeśli wskaźnik ten ma
+ * wartość NULL.
+ * @param[in] pf – wskaźnik na usuwaną strukturę.
+ */
+void phfwdDelete(struct PhoneForward *pf);
+
+/** @brief Dodaje przekierowanie.
+ * Dodaje przekierowanie wszystkich numerów mających prefiks @p num1, na numery,
+ * w których ten prefiks zamieniono odpowiednio na prefiks @p num2. Każdy numer
+ * jest swoim własnym prefiksem. Jeśli wcześniej zostało dodane przekierowanie
+ * z takim samym parametrem @p num1, to jest ono zastępowane.
+ * @param[in] pf   – wskaźnik na strukturę przechowującą przekierowania numerów;
+ * @param[in] num1 – wskaźnik na napis reprezentujący prefiks numerów
+ *                   przekierowywanych;
+ * @param[in] num2 – wskaźnik na napis reprezentujący prefiks numerów, na które
+ *                   jest wykonywane przekierowanie.
+ * @return Wartość @p true, jeśli przekierowanie zostało dodane.
+ *         Wartość @p false, jeśli wystąpił błąd, np. podany napis nie
+ *         reprezentuje numeru, oba podane numery są identyczne lub nie udało
+ *         się zaalokować pamięci.
+ */
+bool phfwdAdd(struct PhoneForward *pf, char const *num1, char const *num2);
+
+/** @brief Usuwa przekierowania.
+ * Usuwa wszystkie przekierowania, w których parametr @p num jest prefiksem
+ * parametru @p num1 użytego przy dodawaniu. Jeśli nie ma takich przekierowań
+ * lub napis nie reprezentuje numeru, nic nie robi.
+ *
+ * @param[in] pf  – wskaźnik na strukturę przechowującą przekierowania numerów;
+ * @param[in] num – wskaźnik na napis reprezentujący prefiks numerów.
+ */
+void phfwdRemove(struct PhoneForward *pf, char const *num);
+
+/** @brief Wyznacza przekierowanie numeru.
+ * Wyznacza przekierowanie podanego numeru. Szuka najdłuższego pasującego
+ * prefiksu. Wynikiem jest co najwyżej jeden numer. Jeśli dany numer nie został
+ * przekierowany, to wynikiem jest ten numer. Jeśli podany napis nie
+ * reprezentuje numeru, wynikiem jest pusty ciąg. Alokuje strukturę
+ * @p PhoneNumbers,która musi być zwolniona za pomocą funkcji @ref phnumDelete.
+ * @param[in] pf  – wskaźnik na strukturę przechowującą przekierowania numerów;
+ * @param[in] num – wskaźnik na napis reprezentujący numer.
+ * @return Wskaźnik na strukturę przechowującą ciąg numerów.
+ */
+struct PhoneNumbers const * phfwdGet(struct PhoneForward *pf, char const *num);
+
+/** @brief Wyznacza przekierowania na dany numer.
+ * Wyznacza wszystkie przekierowania na podany numer. Wynikowy ciąg zawiera też
+ * dany numer. Wynikowe numery są posortowane leksykograficznie i nie mogą się
+ * powtarzać. Jeśli podany napis nie reprezentuje numeru, wynikiem jest pusty
+ * ciąg. Alokuje strukturę @p PhoneNumbers, która musi być zwolniona za pomocą
+ * funkcji @ref phnumDelete.
+ * @param[in] pf  – wskaźnik na strukturę przechowującą przekierowania numerów;
+ * @param[in] num – wskaźnik na napis reprezentujący numer.
+ * @return Wskaźnik na strukturę przechowującą ciąg numerów.
+ */
+struct PhoneNumbers const * phfwdReverse(struct PhoneForward *pf, char const *num);
+
+/** @brief Usuwa strukturę.
+ * Usuwa strukturę wskazywaną przez @p pnum. Nic nie robi, jeśli wskaźnik ten ma
+ * wartość NULL.
+ * @param[in] pnum – wskaźnik na usuwaną strukturę.
+ */
+static inline void phnumDelete(struct PhoneNumbers const *pnum) {
+  free((void*)pnum);
+}
+
+/** @brief Udostępnia numer.
+ * Udostępnia wskaźnik na napis reprezentujący numer. Napisy są indeksowane
+ * kolejno od zera.
+ * @param[in] pnum – wskaźnik na strukturę przechowującą ciąg napisów;
+ * @param[in] idx  – indeks napisu.
+ * @return Wskaźnik na napis. Wartość NULL, jeśli indeks ma za dużą wartość.
+ */
+char const * phnumGet(struct PhoneNumbers const *pnum, size_t idx);
+
+#endif /* __PHONE_FORWARD_H__ */
---
+++ src/phone_forward_example.c
@@ -0,0 +1,117 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "phone_forward.h"
+
+#define MAX_LEN 23
+
+int main() {
+  char num1[MAX_LEN + 1], num2[MAX_LEN + 1];
+  struct PhoneForward *pf;
+  struct PhoneNumbers const *pnum;
+  char const *num;
+  size_t idx;
+  bool result;
+
+  pf = phfwdNew();
+
+  strcpy(num1, "123");
+  strcpy(num2, "9");
+  result = phfwdAdd(pf, num1, num2);
+  assert(result);
+  (void)result;
+
+  pnum = phfwdGet(pf, "1234");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 94.
+  phnumDelete(pnum);
+
+  pnum = phfwdGet(pf, "12");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 12.
+  phnumDelete(pnum);
+
+  strcpy(num1, "123456");
+  strcpy(num2, "777777");
+  result = phfwdAdd(pf, num1, num2);
+  assert(result);
+  (void)result;
+
+  pnum = phfwdGet(pf, "12345");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 945.
+  phnumDelete(pnum);
+
+  pnum = phfwdGet(pf, "123456");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 777777.
+  phnumDelete(pnum);
+
+  pnum = phfwdGet(pf, "997");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 997.
+  phnumDelete(pnum);
+
+  pnum = phfwdReverse(pf, "987654321");
+  idx = 0;
+  // Wypisuje:
+  // 12387654321
+  // 987654321
+  while ((num = phnumGet(pnum, idx++)) != NULL)
+    printf("%s\n", num);
+  phnumDelete(pnum);
+
+  phfwdRemove(pf, "12");
+
+  pnum = phfwdGet(pf, "123456");
+  printf("%s\n", phnumGet(pnum, 0)); // Wypisuje 123456.
+  phnumDelete(pnum);
+
+  pnum = phfwdReverse(pf, "987654321");
+  idx = 0;
+  // Wypisuje:
+  // 987654321
+  while ((num = phnumGet(pnum, idx++)) != NULL)
+    printf("%s\n", num);
+  phnumDelete(pnum);
+
+  result = phfwdAdd(pf, "567", "0");
+  assert(result);
+  (void)result;
+
+  result = phfwdAdd(pf, "5678", "08");
+  assert(result);
+  (void)result;
+
+  pnum = phfwdReverse(pf, "08");
+  idx = 0;
+  // Wypisuje:
+  // 08
+  // 5678
+  while ((num = phnumGet(pnum, idx++)) != NULL)
+    printf("%s\n", num);
+  phnumDelete(pnum);
+
+  result = phfwdAdd(pf, "A", "1");
+  assert(!result);
+  (void)result;
+
+  result = phfwdAdd(pf, "1", "A");
+  assert(!result);
+  (void)result;
+
+  phfwdRemove(pf, "");
+  phfwdRemove(pf, NULL);
+
+  pnum = phfwdGet(pf, "A");
+  assert(phnumGet(pnum, 0) == NULL);
+  phnumDelete(pnum);
+
+  pnum = phfwdReverse(pf, "A");
+  assert(phnumGet(pnum, 0) == NULL);
+  phnumDelete(pnum);
+
+  phfwdDelete(pf);
+
+  pnum = NULL;
+  phnumDelete(pnum);
+  pf = NULL;
+  phfwdDelete(pf);
+
+  return 0;
+}
